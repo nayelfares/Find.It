978320311163f4aed8608635cd41e199d7141137
nayelfares <nayel.fares@shaadoow.com>
2020-12-03 03:29:02 +0400

    search activity

--- app/src/main/java/com/emarketing/findit/ui/Search.kt
+++ app/src/main/java/com/emarketing/findit/ui/Search.kt
@@ -8,27 +8,53 @@ import com.emarketing.findit.R
 import com.emarketing.findit.mvvm.BaseActivity
 import com.emarketing.findit.vm.SearchViewModel
 import kotlinx.android.synthetic.main.activity_search.*
+import java.util.ArrayList
 
 
 class Search : BaseActivity(),SearchView{
     lateinit var searchViewModel: SearchViewModel
+    val allCities=ArrayList<String>()
+    val allCategories=ArrayList<String>()
+    val allTages=ArrayList<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         searchViewModel=SearchViewModel(this, this)
-        val users = arrayOf(
-            "Suresh Dasari",
-            "Trishika Dasari",
-            "Rohini Alavala",
-            "Praveen Kumar",
-            "Madhav Sai"
-        )
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, users)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        city.adapter = adapter
+        initLists()
+    }
+
+    fun initLists(){
+        allCities.add("Select city")
+        allCategories.add("Select category")
+        allTages.add("Select tag")
+        val citiesAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, allCities)
+        citiesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        city.adapter = citiesAdapter
         city.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                showMessage(users[p2])
+                showMessage(allCities[p2])
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+
+        val categoriesAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, allCategories)
+        categoriesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        category.adapter = categoriesAdapter
+        category.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                showMessage(allCategories[p2])
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+        }
+
+        val tagAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, allTages)
+        tagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tag.adapter = tagAdapter
+        tag.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                showMessage(allTages[p2])
             }
             override fun onNothingSelected(p0: AdapterView<*>?) {}
 
